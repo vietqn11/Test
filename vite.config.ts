8cf07887a48c5a9e4ede8ffa017c11392225385c
vietqn11 <nvqntk2@gmail.com>
2025-10-25 19:59:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/test/', // 👈 Thêm dòng này
+        base: '/Test/', // 👈 Thêm dòng này
       server: {
         port: 3000,
         host: '0.0.0.0',
